a459e351befdf0082a8b59bccb64de508d109e9b
ZloyLingvist <35521580+ZloyLingvist@users.noreply.github.com>
2019-01-08 15:59:50 +0500

    Delete fill.php

--- fill.php
+++
@@ -1,8 +0,0 @@
-<?php
-$file = "personal.xlsx"; $handle = fopen($file, "r"); 
-$c = 0; 
-while(($filesop = fgetcsv($handle, 1000, ",")) !== false) { $name = $filesop[0]; $email = $filesop[1]; 
-$sql = mysqli_query("INSERT INTO xls (name, email) VALUES ('$name','$email')"); } if($sql){ echo "You database has imported successfully"; }
-else{ echo "Sorry! There is some problem."; }
-
-?>
